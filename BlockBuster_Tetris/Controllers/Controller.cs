16080edb1c6ec72cf2f2589c2fd0c7886796a463
Daria <dashakukuruza86@gmail.com>
2023-05-06 19:59:38 +0300

    Add project files.

---
+++ BlockBuster_Tetris/Controllers/Controller.cs
@@ -0,0 +1,260 @@
+﻿using BlockBuster_Tetris.Model;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BlockBuster_Tetris.Controllers
+{
+    internal static class Controller
+    {
+        public static Block currentShape;
+        public static int size;
+        public static int[,] map = new int[20, 10];
+        public static int linesRemoved;
+        public static int score;
+        public static int Interval;
+        public static Image img_I = Image.FromFile("Turquoise.png");
+        public static Image img_Z = Image.FromFile("Red.png");
+        public static Image img_T = Image.FromFile("Violet.png");
+        public static Image img_L = Image.FromFile("Green.png");
+        public static Image img_J = Image.FromFile("Orange.png");
+        public static Image img_Square = Image.FromFile("Blue.png");
+        public static Image img_Z_ = Image.FromFile("Yellow.png");
+        public static void ShowNextShape(Graphics e)
+        {
+            for (int i = 0; i < currentShape.sizeNextMatrix; i++)
+            {
+                for (int j = 0; j < currentShape.sizeNextMatrix; j++)
+                {
+                    if (currentShape.nextMatrix[i, j] == 1)
+                    {
+                        e.DrawImage(img_I, new Rectangle(1000 + j * (size) + 1, 90 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 2)
+                    {
+                        e.DrawImage(img_Z, new Rectangle(1000 + j * (size) + 1, 90 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 3)
+                    {
+                        e.DrawImage(img_T, new Rectangle(1000 + j * (size) + 1, 90 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 4)
+                    {
+                        e.DrawImage(img_L, new Rectangle(1000 + j * (size) + 1, 90 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 5)
+                    {
+                        e.DrawImage(img_Square, new Rectangle(1000 + j * (size) + 1, 90 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 6)
+                    {
+                        e.DrawImage(img_Z_, new Rectangle(1000 + j * (size) + 1, 90 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 7)
+                    {
+                        e.DrawImage(img_J, new Rectangle(1000 + j * (size) + 1, 90 + i * (size) + 1, size - 1, size - 1));
+                    }
+                }
+            }
+        }
+
+        public static void ClearMap()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    map[i, j] = 0;
+                }
+            }
+        }
+
+        public static void DrawMap(Graphics e)
+        {
+            int x = (Screen.PrimaryScreen.Bounds.Width - 10 * size) / 2;
+            int y = (Screen.PrimaryScreen.Bounds.Height - 20 * size) / 2;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (map[i, j] == 1)
+                    {
+
+                        e.DrawImage(img_I, new Rectangle(x + j * (size) + 1, y + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 2)
+                    {
+                        e.DrawImage(img_Z, new Rectangle(x + j * (size) + 1, y + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 3)
+                    {
+                        e.DrawImage(img_T, new Rectangle(x + j * (size) + 1, y + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 4)
+                    {
+                        e.DrawImage(img_L, new Rectangle(x + j * (size) + 1, y + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 5)
+                    {
+                        e.DrawImage(img_Square, new Rectangle(x + j * (size) + 1, y + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 6)
+                    {
+                        e.DrawImage(img_Z_, new Rectangle(x + j * (size) + 1, y + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 7)
+                    {
+                        e.DrawImage(img_J, new Rectangle(x + j * (size) + 1, y + i * (size) + 1, size - 1, size - 1));
+                    }
+                }
+            }
+        }
+
+        public static void DrawGrid(Graphics g)
+        {
+            int x = (Screen.PrimaryScreen.Bounds.Width - 10 * size) / 2;
+            int y = (Screen.PrimaryScreen.Bounds.Height - 20 * size) / 2;
+
+            g.FillRectangle(Brushes.DimGray, new Rectangle(x, y, 10 * size, 20 * size));
+
+            for (int i = 0; i <= 20; i++)
+            {
+                g.DrawLine(Pens.Black, new Point(x, y + i * size), new Point(x + 10 * size, y + i * size));
+            }
+            for (int i = 0; i <= 10; i++)
+            {
+                g.DrawLine(Pens.Black, new Point(x + i * size, y), new Point(x + i * size, y + 20 * size));
+            }
+        }
+
+        public static void SliceMap(Label label1, Label label2)
+        {
+            int count = 0;
+            int curRemovedLines = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                count = 0;
+                for (int j = 0; j < 10; j++)
+                {
+                    if (map[i, j] != 0)
+                        count++;
+                }
+                if (count == 10)
+                {
+                    curRemovedLines++;
+                    for (int k = i; k >= 1; k--)
+                    {
+                        for (int o = 0; o < 10; o++)
+                        {
+                            map[k, o] = map[k - 1, o];
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < curRemovedLines; i++)
+            {
+                score += 10 * (i + 1);
+            }
+            linesRemoved += curRemovedLines;
+
+            if (linesRemoved % 5 == 0)
+            {
+                if (Interval > 60)
+                    Interval -= 10;
+            }
+
+            label1.Text = "Score: " + score;
+            label2.Text = "Lines: " + linesRemoved;
+        }
+
+        public static bool IsIntersects()
+        {
+            for (int i = currentShape.y; i < currentShape.y + currentShape.sizeMatrix; i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (j >= 0 && j <= 9)
+                    {
+                        if (map[i, j] != 0 && currentShape.matrix[i - currentShape.y, j - currentShape.x] == 0)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static void Merge()
+        {
+            for (int i = currentShape.y; i < currentShape.y + currentShape.sizeMatrix; i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (currentShape.matrix[i - currentShape.y, j - currentShape.x] != 0)
+                        map[i, j] = currentShape.matrix[i - currentShape.y, j - currentShape.x];
+                }
+            }
+        }
+
+        public static bool Collide()
+        {
+            for (int i = currentShape.y + currentShape.sizeMatrix - 1; i >= currentShape.y; i--)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (currentShape.matrix[i - currentShape.y, j - currentShape.x] != 0)
+                    {
+                        if (i + 1 == 20)
+                            return true;
+                        if (map[i + 1, j] != 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool CollideHor(int dir)
+        {
+            for (int i = currentShape.y; i < currentShape.y + currentShape.sizeMatrix; i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (currentShape.matrix[i - currentShape.y, j - currentShape.x] != 0)
+                    {
+                        if (j + 1 * dir > 9 || j + 1 * dir < 0)
+                            return true;
+
+                        if (map[i, j + 1 * dir] != 0)
+                        {
+                            if (j - currentShape.x + 1 * dir >= currentShape.sizeMatrix || j - currentShape.x + 1 * dir < 0)
+                            {
+                                return true;
+                            }
+                            if (currentShape.matrix[i - currentShape.y, j - currentShape.x + 1 * dir] == 0)
+                                return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static void ResetArea()
+        {
+            for (int i = currentShape.y; i < currentShape.y + currentShape.sizeMatrix; i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (i >= 0 && j >= 0 && i < 20 && j < 10)
+                    {
+                        if (currentShape.matrix[i - currentShape.y, j - currentShape.x] != 0)
+                        {
+                            map[i, j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BlockBuster_Tetris/View.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using BlockBuster_Tetris.Model;
+using BlockBuster_Tetris.Controllers;
+
+
+namespace BlockBuster_Tetris
+{
+    public partial class View : Form
+    {
+        string playerName;
+        private PictureBox pauseImage;
+        public View()
+        {
+            InitializeComponent();
+            playerName = Microsoft.VisualBasic.Interaction.InputBox("Введите имя игрока", "Настройка игрока", "Новый игрок");
+            if (playerName == "")
+            {
+                playerName = "Новый игрок";
+            }
+
+            pauseImage = new PictureBox();
+            pauseImage.ImageLocation = "pause.png";
+            pauseImage.Anchor = AnchorStyles.None;
+            pauseImage.Size = new Size(300, 250); 
+            pauseImage.Location = new Point((this.ClientSize.Width - pauseImage.Width) / 2, (this.ClientSize.Height - pauseImage.Height) / 2);
+            pauseImage.Visible = false;
+            pauseImage.BackColor = Color.Transparent;
+            this.Controls.Add(pauseImage);
+
+            this.KeyUp += new KeyEventHandler(keyFunc);
+            Init();
+        }
+        public void Init()
+        {
+
+            this.Text = "Тетрис: Текущий игрок - " + playerName;
+            Controller.size = 35;
+            Controller.score = 0;
+            Controller.linesRemoved = 0;
+            Controller.currentShape = new Block(3, 0);
+            Controller.Interval = 1000;
+            label1.Text = "Score: " + Controller.score;
+            label2.Text = "Lines: " + Controller.linesRemoved;
+
+            WindowState = FormWindowState.Maximized;
+            // Загрузить картинку
+            Image backgroundImage = Image.FromFile("Fon.jpg");
+
+            // Установить изображение в качестве фона формы
+            this.BackgroundImage = backgroundImage;
+
+            timer1.Interval = Controller.Interval;
+            timer1.Tick += new EventHandler(update);
+            timer1.Start();
+
+
+            Invalidate();
+        }
+
+        private void keyFunc(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+
+                    if (!Controller.IsIntersects())
+                    {
+                        Controller.ResetArea();
+                        Controller.currentShape.RotateBlock();
+                        Controller.Merge();
+                        Invalidate();
+                    }
+                    break;
+                case Keys.Space:
+                    timer1.Interval = 10;
+                    break;
+                case Keys.Right:
+                    if (!Controller.CollideHor(1))
+                    {
+                        Controller.ResetArea();
+                        Controller.currentShape.MoveRight();
+                        Controller.Merge();
+                        Invalidate();
+                    }
+                    break;
+                case Keys.Left:
+                    if (!Controller.CollideHor(-1))
+                    {
+                        Controller.ResetArea();
+                        Controller.currentShape.MoveLeft();
+                        Controller.Merge();
+                        Invalidate();
+                    }
+                    break;
+
+                case Keys.Escape:
+                    if (timer1.Enabled)
+                    {
+                        pauseImage.Visible = true;
+                        timer1.Stop();
+                    }
+                    else
+                    {
+                        pauseImage.Visible = false;
+                        timer1.Start();
+                    }
+                    break;
+            }
+        }
+
+
+        private void update(object sender, EventArgs e)
+        {
+            Controller.ResetArea();
+            if (!Controller.Collide())
+            {
+                Controller.currentShape.MoveDown();
+            }
+            else
+            {
+                Controller.Merge();
+                Controller.SliceMap(label1, label2);
+                timer1.Interval = Controller.Interval;
+                Controller.currentShape.ResetBlock(3, 0);
+                if (Controller.Collide())
+                {
+                    Controller.ClearMap();
+                    timer1.Tick -= new EventHandler(update);
+                    timer1.Stop();
+                    MessageBox.Show("Ваш результат: " + Controller.score);
+                    Init();
+                }
+            }
+            Controller.Merge();
+            Invalidate();
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Controller.DrawGrid(e.Graphics);
+            Controller.DrawMap(e.Graphics);
+            Controller.ShowNextShape(e.Graphics);
+        }
+
+        private void OnAgainButtonClick(object sender, EventArgs e)
+        {
+            timer1.Tick -= new EventHandler(update);
+            timer1.Stop();
+            Controller.ClearMap();
+            Init();
+        }
+    }
+}
